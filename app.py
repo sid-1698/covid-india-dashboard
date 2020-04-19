b5aa687dee7c4c281cc76dfdaf00d87ac1edd861
Sidarth Sudhakar <sidarth.sudhakar98@gmail.com>
2020-04-20 02:44:06 +0530

    server

--- app.py
+++ app.py
@@ -291,4 +291,4 @@ def render_state_geo(hoverData):
         return geoplot.plot2(path,hoverData['points'][0]['location'])
 
 if __name__ == "__main__":
-    app.run_server()  
+    app.run_server(debug=True)  
